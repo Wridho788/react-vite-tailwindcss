2515e502537b4593bc6c6961c62f57767c165188
Wridho788 <wridho246@gmail.com>
2023-02-17 16:10:08 +0700

    extract component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
-import { IconBrandTwitter, IconBrandFacebook, IconBrandGithub } from '@tabler/icons-react'
+import PlaceContentCenter from './components/PlaceContentCenter'
 import Button from './components/Button'
-import Card from './components/Card'
+import { useState } from 'react'
+import Counter from './components/Counter'
 const App = () => {
+
+
     return (
-        <div className='bg-slate-100 text-slate-800 tracking-tighter flex items-center justify-center antialiased min-h-screen'>
-            <div className='max-w-md w-full'>
-                <Card>
-                    <Card.Title>Hello World</Card.Title>
-                    <Card.Body>
-                        {' '}
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet asperiores voluptas quos dolores delectus accusamus
-                        distinctio rerum praesentium, commodi assumenda excepturi suscipit cupiditate dolorem nisi reiciendis repellendus facere,
-                        accusantium odit?{' '}
-                    </Card.Body>
-                    <Card.Footer>
-                        <Button>Register</Button>
-                    </Card.Footer>
-                </Card>
-            </div>
-        </div>
+        <PlaceContentCenter>
+            <Counter initialValue={10} />
+        </PlaceContentCenter>
     )
 }
 
