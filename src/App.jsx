c806c11f6dbe855732585d5ca833ddca968034ae
Wridho788 <wridho246@gmail.com>
2023-02-21 11:06:25 +0700

    basic useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import PlaceContentCenter from './components/PlaceContentCenter'
-import Todo from './components/Todo'
+import Card from './components/Card'
+import Button from './components/Button'
+import Input from './components/Input'
+import { useRef, useState } from 'react'
 
 const App = () => {
+    // const [tick, setTick] = useState(0)
+    // const inputRef = useRef(null)
 
+    function handleClick() {
+        // inputRef.current.focus()
+        // const newTick = tick + 1
+        // setTick(newTick)
+        // console.log(newTick)
+    }
     return (
         <PlaceContentCenter>
-            <Todo />
+            <Card>
+                <Card.Title>useRef Hooks </Card.Title>
+                <Card.Body>
+                    <Input className="boder border-slate-600" isFocused/> 
+                    <Button text='Tick' onClick={handleClick} />
+                </Card.Body>
+                {/* <Card.Footer>You clicked {tick} times</Card.Footer> */}
+            </Card>
         </PlaceContentCenter>
     )
 }
