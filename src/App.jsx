96ff3cdf8669df47a7ce6967cba551414dfa7166
Wridho788 <wridho246@gmail.com>
2023-02-21 15:44:57 +0700

    ajax and api

--- src/App.jsx
+++ src/App.jsx
@@ -2,52 +2,51 @@ import PlaceContentCenter from './components/PlaceContentCenter'
 import Input from './components/Input'
 import Button from './components/Button'
 import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Card from './components/Card'
 
 const App = () => {
+    const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(false)
 
-    const [name, setName] = useState('')
-    const [online, setOnline] = useState(false)
-
-    // ! Hooks always rendered
-    useEffect( () => {
-        // console.log('always rendered')
-    })
-
-    // ! Hooks First Render
-    useEffect( () => {
-        // console.log('first rendered')
-    },[])
-
-    // ! Hooks rendered after if change state
-    useEffect( () => {
-        // console.log(`i am now ${online ? 'online' : 'offline'}`)
-    },[online])
-
-    // ! Hooks cleanup hook
-    // example
-    function updateScrollPosition() {
-        const windowScrolling = window.scrollY
-        console.log(`window scrolling ${windowScrolling}`)
-        setScrollPosition(windowScrolling)
-    }
-
-
-    useEffect( () => {
-        window.addEventListener('scroll', updateScrollPosition)
-        // TODO: dibagian ini dibutuhkan cleanup function
-        return () => {
-            window.removeEventListener('scroll', updateScrollPosition)
+    useEffect(() => {
+        async function getUsers() {
+            setLoading(true)
+            try {
+                const response = await axios('https://jsonplaceholder.typicode.com/users')
+                setUsers(response.data)
+                setLoading(false)
+                console.log('====================================')
+                console.log('user', response)
+                console.log('====================================')
+            } catch (e) {
+                console.log(e.message)
+            }
         }
-    })
 
-
+        getUsers()
+    }, [])
     return (
-        <div className='h-[4000px]'>
-            <Input className="boder border-slate-600" value={name} onChange={(e)=> setName(e.target.value)}/>
-            <Button onClick={(e)=> setOnline(online=> !online)} >Set Online</Button>
-
-
-        </div>
+        <PlaceContentCenter>
+            <Card>
+                <Card.Title>Users: {users.length}</Card.Title>
+                <Card.Body>
+                    {loading ? (
+                        <div>loading .... </div>
+                    ) : users.length ? (
+                        <ol>
+                            {users.map((user) => (
+                                <li key={user.id}>
+                                    {user.name} {user.username}
+                                </li>
+                            ))}
+                        </ol>
+                    ) : (
+                        <div>There are no users</div>
+                    )}
+                </Card.Body>
+            </Card>
+        </PlaceContentCenter>
     )
 }
 
